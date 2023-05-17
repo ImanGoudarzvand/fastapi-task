91e58d4db09d536b2cca6e27b8b8ae03f462cc5e
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-17 18:31:56 +0330

    initialize database tables

--- app/main.py
+++ app/main.py
@@ -3,11 +3,16 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
+from app.db.base import Base
+from app.db.session import engine
+
 
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
 # , openapi_url=f"{settings.API_V1_STR}/openapi.json"
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
