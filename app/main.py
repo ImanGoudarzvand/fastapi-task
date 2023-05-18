5cd4f7389ead8892baca3cf824489f0e55232f7a
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-18 16:36:40 +0330

    add a exception handler for unprocessible entity

--- app/main.py
+++ app/main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
 from app.api.api_v1.api import api_router
 from app.core.config import settings
-from app.db.base import Base
+from app.db.base_class import Base
 from app.db.session import engine
-
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 app = FastAPI(
     title=settings.PROJECT_NAME
 )
-# , openapi_url=f"{settings.API_V1_STR}/openapi.json"
+
+
+# add this for handling Unprocessible entity erros (422) -> 400
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+
+    return JSONResponse(status_code=400, content={"errors": "Invalid data"})
+
 
 Base.metadata.create_all(bind=engine)
+
+
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
